9e6e85cfbfd357e4983c1e5f8656540bd2ceac02
HendrikFito <darksolj@live.com>
2019-07-24 14:49:11 +0200

    Clean up, saved dotstring in text file

--- index.js
+++ index.js
@@ -1,38 +1,145 @@
 const fs = require('fs');
-const program = require('commander');
+const path = require('path');
 
+const readline = require('readline');
+const program = require('commander');
+//const linereader = require('line-reader');
+const importRegEx = new RegExp(/(?:import[\s*as{}]+)(?=\b)([A-z,]+)/);
+const importPathRegEx = new RegExp(/((?:from[\s'"]+)|(?:require[\('"]+))([\.A-z/@]+)/);
+const classRegExp = new RegExp(/(?:class[\s]+)([A-z]+)/, 'g');
+
+let dotstring = '';
+
+
+const parseFileImports = (file) => {
+    const importmap = [];
+    const filecontent = fs.readFileSync(file);
+
+    const filecontentlines = filecontent.toString().split(/\r|\n/g);
+    for (var i = 0, l = filecontentlines.length; i < l;i++) {
+        const line = filecontentlines[i];
+        if (line.match(/^(import).+(from|require)/)) {
+            const impClass = importRegEx.exec(line);
+            const impFrom = importPathRegEx.exec(line);
+             importmap[impClass[1].trim()] = impFrom[2];
+        }
+    }
+    return importmap;
+}
 
 class ComponentFile {
-    constructor(filename, classname) {
-        this.filename = filename;
-        this.classname = classname;
+    constructor(filename) {
+        this.filename = filename; 
+        this.importedBy = [];
+        this.imports = [];
+        this.classname = this.getClassName();
+        this.getImports();
     }
 
-    match(filename) {
+    getImports() {
+        const imports = parseFileImports(this.filename);
+        this.imports = imports;
+    }
+
+    getClassName() {
+        const filecontent = fs.readFileSync(this.filename);
+        const classname = classRegExp.exec(filecontent);
+        if (classname === null) {
+
+            return path.basename(this.filename);
+        } else {
+            return classname[1];
+        }
+    }
+
+    matchFile(filename) {
         return this.filename === filename; 
     }
 
-    match(classname) {
-        return this.classname === classname;
+    setImportedBy(file) {
+        if (!this.importedBy.includes(file)) {
+            this.importedBy.push(file);
+        }
+    }
+
+    getDot() {
+        let dotstring = '';
+        for (let file in this.imports) {
+            const dot = `${this.classname}->${file};`;
+            dotstring+=dot;
+        }
+        return dotstring;
     }
 }
 
+class ComponentFolder {
+    constructor(foldername) {
+        this.folder = foldername;
+        this.filelist = [];
+        this.folderlist = [];
+        this.getFileList();
+    }
+
+    getFileList() {
+        const foldercontent = fs.readdirSync(this.folder)
+        for (let i = 0; i < foldercontent.length; i++) {
+            const content = foldercontent[i];
+            //console.log("Processing: ", content);
+            if (fs.statSync(this.folder + '/' + content).isDirectory()) {
+                this.folderlist.push(new ComponentFolder(this.folder + '/' + content));
+            } else {
+                this.filelist.push(new ComponentFile(this.folder + '/' + content));
+            }
+        }   
+    }
+
+    getDot() {
+        let dotstring = '';
+        this.filelist.forEach( file => {
+            dotstring += file.getDot();
+        });
+
+        this.folderlist.forEach( folder => {
+            dotstring += folder.getDot();
+        });
+        return dotstring;
+    }
+
+
+}
+
+
 program
     .version('0.0.1')
-    .option('-P', '--path', 'Path to React Project that should be mapped out')
-    .option('-V', '--verbose', 'Verbose option for extra loggin sugarz')
-
-
+    .option('-p --path <path>', 'Path to React Project that should be mapped out')
+    .option('-v --verbose', 'Verbose option for extra loggin sugarz')
 
 const main = () => {
+    program.parse(process.argv);
+    //console.log(program.path);
     const filehashmap = [];
-    
-    
-    
+    const folderMap = [];
+    let folderList = [];
+    try {
+        const folders = fs.readdirSync(program.path);                
+        const folderlistlength = folders.length;
+        for (let i = 0; i < folderlistlength; i++) {
+            folderMap.push(
+                new ComponentFolder(program.path + '/' + folders[i])
+            )
+        }
 
-
-
-
+        let dotstring = '';
+        for (let i = 0; i < folderMap.length; i++) {
+            dotstring += folderMap[i].getDot();
+        }
+        dotstring = dotstring.replace(/,/g,'');
+        fs.writeFileSync('dotstring.txt', dotstring);
+    } catch (e) {
+        console.error(e);
+    }
 };
 
 main();
+
+//setTimeout(() => main(), 5000);
